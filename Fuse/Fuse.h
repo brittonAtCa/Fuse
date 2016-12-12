817df914caa0f8552f781a5424b38b9eed3229c9
Britton Katnich <katnich@shaw.ca>
2016-12-11 16:22:25 -0800

    Initial Commit

---
+++ Fuse/Fuse.h
@@ -0,0 +1,19 @@
+//
+//  Fuse.h
+//  Fuse
+//
+//  Created by Britton Katnich on 2016-12-11.
+//  Copyright © 2016 Stratum Dynamics Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fuse.
+FOUNDATION_EXPORT double FuseVersionNumber;
+
+//! Project version string for Fuse.
+FOUNDATION_EXPORT const unsigned char FuseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fuse/PublicHeader.h>
+
+
